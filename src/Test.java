0298c661649a40d1f594cae3746d4c4896e88582
manoj <manoj.sarwate@ssiedu.in>
2019-09-28 17:48:59 +0530

    28-09-2019

--- src/Test.java
+++ src/Test.java
@@ -1,34 +1,36 @@
 
 public class Test {
 
+    private int x, y;
+    public Test(int a, int b){x=a;y=b;}
+    public void show(){System.out.println(x+","+y);}
     
-    public Test(){
-        this(1);
-        System.out.println("Test-0-1");
-        System.out.println("Test-0-2");
-    }
-    public Test(int p){
-        this(1,2);
-        System.out.println("Test-1-1");
-        System.out.println("Test-1-2");
-    }
-    public Test(int p, int q){
-        System.out.println("Test-2-1");
-        System.out.println("Test-2-2");
-    }
-    
-    public void f1() {
-        System.out.println("hello user");
-        System.out.println("how are you");
-    }
-
-    public void f2() {
-        f1();
-        System.out.println("welcome to indore");
-    }
-
     public static void main(String[] args) {
-        Test t=new Test();
+        Test t1;
+        new Test(10,20);
+        Test t2=new Test(30,40);
+        Test t3=t2;
+        t1=t3;
+        t1.show();
+        t2.show();
+        t3.show();
+         
+        System.out.println("_______________________________");
+        t3=new Test(50,60);
+        t1.show();
+        t2.show();
+        t3.show();
+        System.out.println("_______________________________");
+        
+        
+        t3=t2;
+        
+        t1.show();
+        t2.show();
+        t3.show();
+        System.out.println("_______________________________");
+        
+        
         
     }
 
